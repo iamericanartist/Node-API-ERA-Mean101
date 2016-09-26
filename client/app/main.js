d67e6a3f0d19545a5f2b754ba58fbaaf9e5c2118
Eric Denton <eric@iamericanartist.com>
2016-09-26 10:57:00 -0500

    added bootstrap and getting title from Node

--- client/app/main.js
+++ client/app/main.js
@@ -2,6 +2,13 @@
 
 angular
   .module('mean101', [])
-  .controller('main', function ($scope) {
-    $scope.title = 'MEAN 101 from Angular'
+  .controller('main', function ($scope, $http) {
+    $http
+      .get("/api/title")
+      // .then((data) => 
+      //   $scope.title = data.data.title
+     //destructured
+      .then(({ data: { title }}) =>
+        $scope.title = title
+      )
   })
--- server/server.js
+++ server/server.js
@@ -7,4 +7,9 @@ const port = process.env.PORT || 3000
 //express base directory is the ROOT, not the folder where the server is - established in package.JSON
 app.use(express.static('client'))
 
+app.get("/api/title", (req, res) =>
+  // res.send({ title: "MEAN 101 from Node!" })  //use objects here NOT STRINGS 
+  res.json({ title: "MEAN 101 from Node!" })  //use objects here NOT STRINGS 
+)
+
 app.listen(port, () => console.log(`Listening on port: ${port}`))
